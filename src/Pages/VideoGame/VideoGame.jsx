47bfbfb079a8f0e7560230d29626b5fef18e5de9
johalama2023 <johalama2023@gmail.com>
2024-06-03 11:44:41 -0500

    actualizacion

--- src/Pages/VideoGame/VideoGame.jsx
+++ src/Pages/VideoGame/VideoGame.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const VideoGame = () => {
   return (
-    <div className=" flex flex-col justify-between md:justify-center items-center bg-[url('/bg-game.png')] bg-cover bg-center h-screen">
+    <div className=" flex flex-col justify-center items-center bg-[url('/bg-main-reverse.png')] bg-cover bg-center h-screen">
       <div className="flex flex-col justify-center items-center">
         <h1 className=" text-3xl md:text-7xl md:tracking-wide text-[#D31010] text-shadow-white text-center font-bold pt-20 ">
           AMARA LA MUÑECA POSEIDA:{" "}
           <span className="text-[#D31010] text-shadow-white">EL DESPERTAR</span>
         </h1>
       </div>
-      <div className=" p-4 mt-4 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm md:w-[50%]">
+      <div className="flex flex-col md:flex-row items-center p-4 mt-4 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm md:w-[70%] ">
+      <img src="/juego.png" alt="movie" className=" w-[75%] md:w-[50%]" />
         <p className="text-white text-base text-justify md:text-4xl text-shadow-red">
           Los jugadores tomarán el control de las hermanas para poder escapar
           fuera de la ciudad y esconderse de la policía ellas podrán esconderse
--- src/Pages/WebSeries/WebSeries.jsx
+++ src/Pages/WebSeries/WebSeries.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const WebSeries = () => {
   return (
     <div className=" flex flex-col justify-center items-center bg-[url('/bg-main-reverse.png')] bg-cover bg-center h-screen">
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className=" text-3xl md:text-7xl md:tracking-wide text-white font-bold pt-20 text-shadow-red">HISTORIA <span className="text-[#D31010] text-shadow-white">NUCLEAR</span></h1>
-     
+      <div className="flex flex-col justify-center items-center">
+        <h1 className=" text-3xl md:text-7xl md:tracking-wide text-white font-bold pt-20 text-shadow-red">
+          HISTORIA{" "}
+          <span className="text-[#D31010] text-shadow-white">NUCLEAR</span>
+        </h1>
+      </div>
+      <div className=" p-4 mt-4 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm md:w-[50%]">
+        <p className="text-white text-[15px] text-justify md:text-4xl text-shadow-red">
+          La familia Rodriguez conformada por los padres Kamila Hernandez y
+          Fabio Rodriguez, su hija Mía Rodriguez de 12 años y Alice Rodriguez de
+          19 años quién es muy rebelde y tiene un conflicto con sus padres,
+          ellos deciden mudarse a una casa nueva, al llegar a la casa Mía
+          encuentra una muñeca de felpa llamada Amara en donde habita un
+          demonio, Mía le pregunta a sus padres Kamila Hernandez y Fabio
+          Rodriguez si se puede quedar con la muñeca, ellos convencidos aceptan,
+          rápidamente forma un vínculo con la muñeca y la comienza a poseer,
+          pero al pasar los días Amara posee el cuerpo de Mía para mate a sus
+          padres y a su hermana Alice Rodriguez con su ayuda, así logrando matar
+          a los padres pero menos a la hermana ya que trató de escapar y avisar
+          a la policía, la policía y el psicólogo David Álvarez al no creerle y
+          ellos pensando que en realidad ella había matado a sus padres la
+          llevan con un psiquiatra, así el Juez Gonzalo Velez dando a Mía en
+          adopción con una nueva familia.
+        </p>
+      </div>
     </div>
-    <div className=" p-4 mt-4 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm md:w-[50%]">
-       <p className="text-white text-base text-justify md:text-4xl text-shadow-red">La familia Rodríguez se muda a una casa nueva. Al llegar, Mía, de 7 años, encuentra una muñeca de felpa llamada Amara, que está poseída por un demonio. Mía pide a sus padres, Kamila Hernández y Fabio Rodríguez, quedarse con la muñeca, y ellos aceptan. Rápidamente, Mía forma un vínculo con Amara. Con el tiempo, Amara persuade a Mía para que mate a sus padres y a su hermana, Alice Rodríguez. Mía logra matar a sus padres, pero Alice intenta escapar y avisa a la policía. Sin embargo, la policía y el psicólogo no creen la historia de Alice y piensan que ella es la culpable de los asesinatos. Finalmente, un juez decide que Mía sea adoptada por una nueva familia.</p>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default WebSeries
+export default WebSeries;
