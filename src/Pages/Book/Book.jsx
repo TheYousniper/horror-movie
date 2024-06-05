51df404a38aa793af2920198e7bad0710f4b89a2
johalama2023 <johalama2023@gmail.com>
2024-06-04 20:44:03 -0500

    actualizando

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,18 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import BookModal from "../../components/Modal/BookModal";
 
 const Book = () => {
+  const images = [
+    {
+      name: "Amara",
+      image: "/amara.png",
+    },
+    {
+      name: "Mía Rodríguez",
+      image: "/bookMiaRodriguez.png",
+    },
+    {
+      name: "Alice Rodríguez",
+      image: "/bookAliceRodriguez.png",
+    },
+    {
+      name: "José Perales",
+      image: "/bookJosePerales.png",
+    },
+    {
+      name: "María Coveñas",
+      image: "/bookMariaCovenas.png",
+    },
+    {
+      name: "Hugo Perales",
+      image: "/bookHugoPerales.png",
+    },
+    {
+      name: "Gerardo Ruiz",
+      image: "/bookGerardoRuiz.png",
+    },
+  ];
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div className=" flex flex-col justify-center items-center bg-[url('/bg-main-reverse.png')] bg-cover bg-center h-screen">
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className=" text-3xl md:text-7xl xl:text-6xl 2xl:text-8xl md:tracking-wide text-[#D31010] text-shadow-white text-center font-bold pt-20 ">AMARA LA MUÑECA POSEIDA: <span className="text-[#D31010] text-shadow-white">LA BUSQUEDA</span></h1>
-     
+      <div className="flex flex-col justify-center items-center mt-[-10px]">
+        <h1 className="text-2xl md:text-7xl xl:text-6xl 2xl:text-8xl md:tracking-wide text-[#D31010] text-shadow-white text-center font-bold mt-20 px-2 ">
+          AMARA LA MUÑECA POSEIDA:{" "}
+          <span className="text-[#D31010] text-shadow-white">LA BUSQUEDA</span>
+        </h1>
+      </div>
+      <div className="flex flex-col md:flex-row items-center p-4 mt-4 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm md:w-[70%] xl:w-[80%] ">
+        <img
+          src="/libro.png"
+          alt="movie"
+          className=" w-[75%] md:w-[50%] floating-image"
+        />
+        <p className="text-white text-[14px] text-justify md:text-4xl text-shadow-red xl:text-xl 2xl:text-4xl">
+          Han pasado 5 años desde lo sucedido con Mía Rodríguez. Ahora vive en
+          una nueva casa con sus padres adoptivos, José Perales y María Coveñas,
+          y su hermanastro, Hugo Perales. Con el tiempo, Alice es dada de alta y
+          visita a su hermana Mía, gracias a la intervención de su psicólogo,
+          Gerardo Ruiz, quien ha estado tratando su caso. Amara, la muñeca,
+          intenta nuevamente convencer a Mía y ahora también a Alice de asesinar
+          a su hermanastro. Esta vez, la muñeca no solo manipula a Mía, sino
+          también a Alice, logrando que ambas maten a Hugo. Después del
+          asesinato, las hermanas Rodríguez escapan de la casa.
+        </p>
+        <button
+          className="text-white md:text-2xl p-2 mx-2 rounded-lg bg-[#D31010] hover:bg-white hover:text-[#D31010] duration-200 ease"
+          onClick={() => setOpen(true)}
+        >
+          Personajes
+        </button>
     </div>
-    <div className="flex flex-col md:flex-row items-center p-4 mt-4 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm md:w-[70%] xl:w-[80%] ">
-    <img src="/libro.png" alt="movie" className=" w-[75%] md:w-[50%] floating-image" />
-       <p className="text-white text-[15px] text-justify md:text-4xl text-shadow-red xl:text-2xl 2xl:text-4xl">Han pasado 5 años desde lo sucedido con Mía Rodríguez. Ahora vive en una nueva casa con sus padres adoptivos, José Perales y María Coveñas, y su hermanastro, Hugo Perales. Con el tiempo, Alice es dada de alta y visita a su hermana Mía, gracias a la intervención de su psicólogo, Gerardo Ruiz, quien ha estado tratando su caso. Amara, la muñeca, intenta nuevamente convencer a Mía y ahora también a Alice de asesinar a su hermanastro. Esta vez, la muñeca no solo manipula a Mía, sino también a Alice, logrando que ambas maten a Hugo. Después del asesinato, las hermanas Rodríguez escapan de la casa.</p>
-    </div>
-  </div>
-  )
-}
+        <BookModal open={open} onClose={() => setOpen(false)}>
+            <div className="grid grid-cols-2 gap-4 xl:flex xl:flex-wrap xl:justify-center xl:items-center">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className={`flex flex-col justify-start items-center rounded-xl mt-1 ${
+                    index === images.length - 1 ? "col-span-2" : ""
+                  }`}
+                >
+                  <img
+                    src={image.image}
+                    alt={image.name}
+                    className="w-[80px] h-[80px] xl:w-[180px] xl:h-[200px] object-cover rounded-xl border border-red-900"
+                  />
+                  <h2 className="text-[#D31010] text-base md:text-2xl text-center">
+                    {image.name}
+                  </h2>
+                </div>
+              ))}
+            </div>
+          </BookModal>
+      </div>
+  );
+};
 
-export default Book
+export default Book;
---
+++ src/components/Modal/GameModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaTimes } from "react-icons/fa";
+
+const GameModal = () => {
+  return (
+    <div
+      onClick={onClose}
+      className={`fixed inset-0 flex justify-center items-center transition-colors
+      ${open ? "visible bg-black/20" : "invisible"}
+      `}
+    >
+      {/* Modal */}
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`bg-white rounded-xl shadow p-6 transition-all ${
+          open ? "scale-100 opacity-100" : "scale-125 opacity-0"
+        }`}
+      >
+        <button
+        onClick={onClose}
+         className="absolute top-2 right-2 p-1 rounded-lg text-[#D31010] text-xl bg-white hover:bg-[#D31010] hover:text-white transition duration-300 ease-in-out">
+          <FaTimes />
+        </button>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default GameModal
